a0f919f6e8e5e6c30596fd981b5f9f151a392997
cparker-uc <parkecp@mail.uc.edu>
2025-12-11 10:28:44 -0500

    Added script to set FT232H serial number

---
+++ set_ft232h_serial.py
@@ -0,0 +1,44 @@
+# File Name: dump_ftdi_eeprom.py
+# Author: Christopher Parker
+# Created: Tue Apr 22, 2025 | 09:29P EDT
+# Last Modified: Tue Apr 22, 2025 | 09:36P EDT
+
+"""Takes a serial number as argument on CLI and sets it to the connected
+FTDI device (make sure there is only one plugged in when running this!"""
+
+import sys
+from pyftdi import eeprom
+
+eeprom_ = eeprom.FtdiEeprom()
+eeprom_.open('ftdi:///1')
+
+eeprom_.dump_config()
+
+eeprom_.set_serial_number(sys.argv[1] if sys.argv[1] else 'undefined')
+eeprom_.commit(dry_run=False)
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#                                 MIT License                                 #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+#     Copyright (c) 2022 Christopher John Parker <parkecp@mail.uc.edu>        #
+#                                                                             #
+# Permission is hereby granted, free of charge, to any person obtaining a     #
+# copy of this software and associated documentation files (the "Software"),  #
+# to deal in the Software without restriction, including without limitation   #
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,    #
+# and/or sell copies of the Software, and to permit persons to whom the       #
+# Software is furnished to do so, subject to the following conditions:        #
+#                                                                             #
+# The above copyright notice and this permission notice shall be included in  #
+# all copies or substantial portions of the Software.                         #
+#                                                                             #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,    #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING     #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER         #
+# DEALINGS IN THE SOFTWARE.                                                   #
+#                                                                             #
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
